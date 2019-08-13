c462e1776dcb4cd37c457fac08db73d75004cb39
Adonis YIMGA <careladonis@gmail.com>
2019-08-13 18:58:03 +0100

    my project setup successfully

---
+++ shppingbackend/src/main/java/ado/net/shppingbackend/App.java
@@ -0,0 +1,13 @@
+package ado.net.shppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
